6bbb1b49773d491362f369979f6bd1b7e80bdfe4
Janon000 <brictonijunk@gmail.com>
2023-07-23 18:57:18 -0500

    Complete change up of layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,27 @@
-"use client"
+"use client";
 import Link from "next/link";
 
-function Header() {
+function Header({ isLogin }: { isLogin?: boolean }) {
   return (
-    <div className="hidden sm:flex sticky top-0 w-full">
-      <div
-        id="left"
-        className="flex w-[250px] bg-[#0E2162] h-[56px] text-white items-center"
-      >
-        <div className="px-8">FOG SERVER</div>
-      </div>
-      <div className="flex flex-grow bg-[#0E2162]  h-[56px] justify-end items-center text-white">
-        <Link href={"/login"} className="mx-4">
-          Log Out
-        </Link>
-      </div>
-    </div>
+    <>
+      {isLogin ? (
+        ""
+      ) : (
+        <div className="hidden sm:flex sticky top-0 w-full">
+          <div
+            id="left"
+            className="flex w-[250px] bg-[#0E2162] h-[56px] text-white items-center"
+          >
+            <div className="px-8">FOG SERVER</div>
+          </div>
+          <div className="flex flex-grow bg-[#0E2162]  h-[56px] justify-end items-center text-white">
+            <Link href={"/login"} className="mx-4">
+              Log Out
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
